1220e8f01e423aec143dd5421492396729df3fba
Wesley Lins <63325019+wesli10@users.noreply.github.com>
2021-07-09 16:27:23 -0300

    Add files via upload

---
+++ Python/config.py
@@ -0,0 +1,140 @@
+import requests as rq
+import webbrowser as web
+
+version = "1.0.0"
+cidade = 'joinville'
+
+def intro():
+    msg = "Assistente - version {} / by: Wesley Lins".format(version)
+    print("-" * len(msg) + "\n{}\n".format(msg) + "-" * len(msg)) 
+
+lista_erros = [
+    "Não entendi nada",
+    "Desculpe, não entendi",
+    "Repita por gentileza"
+]
+
+conversas = {
+    "Olá": "oi, tudo bem?",
+    "sim e você": "Estou bem obrigada por perguntar",
+    "suave": "Tudo tranquilo mestre",
+    "como está": "O dia ta lindo, clima ensorarado"
+    
+}
+
+comandos = {
+    "desligar": "desligando",
+    "reiniciar": "reiniciando"
+}
+
+def verifica_nome(user_name):
+    if user_name.startswith("meu nome é"):
+        user_name = user_name.replace("meu nome é", "")
+    
+    if user_name.startswith("eu me chamo"):
+        user_name = user_name.replace("eu me chamo", "")
+
+    if user_name.startswith("eu sou o"):
+        user_name = user_name.replace("eu sou o", "")
+
+    if user_name.startswith("eu sou a"):
+        user_name = user_name.replace("eu sou a", "")
+
+    return user_name
+
+
+def verifica_nome_existe(nome):
+    dados = open("dados/nomes.txt", "r")
+    nome_list = dados.readlines()
+
+    if not nome_list:
+        vazio = open("dados/nomes.txt", "r")
+        conteudo = vazio.readlines()
+        conteudo.append("{}".format(nome))
+        vazio = open("dados/nomes.txt", "w")
+        vazio.writelines(conteudo)
+        vazio.close
+
+        return "Olá {}, prazer em te conhecer!".format(nome)
+
+    for linha in nome_list:
+        if linha == nome:
+            return "Olá {}, qual a boa ?".format(nome)
+
+    vazio = open("dados/nomes.txt", "r")
+    conteudo = vazio.readlines()
+    conteudo.append("\n{}".format(nome))
+    vazio = open("dados/nomes.txt", "w")
+    vazio.writelines(conteudo)
+    vazio.close
+    
+    return "Oi {} é a primera vez que nos falamos".format(nome)
+
+
+def name_list():
+    try:
+        nomes = open("dados/nomes.txt", "r")
+        nomes.close
+    except FileNotFoundError:
+        nomes = open("dados/nomes.txt", "w")
+        nomes.close
+
+def calcula(entrada):
+    if "mais" in entrada or "+" in entrada:
+        entrada_recebidas = entrada.split(" ")
+        resultado = int(entrada_recebidas[1]) + int(entrada_recebidas[3])
+    elif "menos" in entrada or "-" in entrada:
+        entrada_recebidas = entrada.split(" ")
+        resultado = int(entrada_recebidas[1]) - int(entrada_recebidas[3])
+        
+    elif "vezes" in entrada or "x" in entrada:
+        entrada_recebidas = entrada.split(" ")
+        resultado = round(float(entrada_recebidas[1]) * float(entrada_recebidas[3]), 2)
+
+    elif "dividido" in entrada or "/" in entrada:
+
+        entrada_recebidas = entrada.split(" ")
+        resultado = round(float(entrada_recebidas[1]) / float(entrada_recebidas[4]), 2)
+    else:
+        resultado = "Operação não encontrada"
+
+    return resultado
+
+
+def clima_tempo():
+    
+    endereco_api = "http://api.openweathermap.org/data/2.5/weather?appid=a2b5dc2df428fbf233b09e3e07376119&q"
+    url = endereco_api + cidade
+
+    infos = rq.get(url).json()
+
+    longitude = infos['coord']['lon']
+    latitude = infos['coord']['lat']
+    temp = infos['main']['temp'] - 273.15 # Kelvin para Celcius
+    pressao_atm = infos['main']['pressure'] / 1013.25 #Libras para ATM
+    humidade = infos['main']['humidity'] #Recebe em porcentagem
+    temp_max = infos['main']['temp_max'] - 273.15 # Kelvin para Celcius
+    temp_min = infos['main']['temp_min'] - 273.15 # Kelvin para Celcius
+    v_speed = infos['wind']['speed']
+    v_direc = infos['wind']['deg']
+    nebulosidade = infos['clouds']['all']
+    id_da_cidade = infos['id']
+
+    return [longitude, latitude, temp,
+            pressao_atm, humidade,
+            temp_max, temp_min,
+            v_speed, v_direc,
+            nebulosidade, id_da_cidade]
+
+def temperatura():
+
+    temp_atual = clima_tempo()[2]
+    temp_max = clima_tempo()[5]
+    temp_min = clima_tempo()[6]
+    return [temp_atual, temp_max, temp_min]
+
+# def abrir(fala):
+#     try:
+#         if "google" in fala:
+#             web.get(caminho_navegador).open("google.com.br/")
+
---
+++ Python/main.py
@@ -0,0 +1,128 @@
+import speech_recognition as sr
+import pyttsx3
+from config import *
+from random import choice
+
+
+
+
+
+reproducao = pyttsx3.init()
+voices = reproducao.getProperty('voices')
+reproducao.setProperty('voices', voices[1].id)
+
+
+def sai_som(resposta):
+    reproducao.say(resposta)
+    reproducao.runAndWait()
+
+def assistente(): 
+    print("Oi, qual é o  seu nome completo")
+    sai_som("Eu me chamo Fritz, qual é o seu nome completo")
+    while True:
+        resposta_erro_aleatoria = choice(lista_erros)
+        rec = sr.Recognizer()
+        with sr.Microphone() as s:
+            rec.adjust_for_ambient_noise(s)
+
+            while True:
+                try:
+                    audio = rec.listen(s)
+                    user_name = rec.recognize_google(audio, language="pt")
+                    user_name = verifica_nome(user_name)
+                    name_list()
+                    apresentacao = "{}".format(verifica_nome_existe(user_name))
+                    print(apresentacao)
+                    sai_som(apresentacao)
+
+                    brute_user_name = user_name
+                    user_name = user_name.split(" ")
+                    user_name = user_name[0]
+                    break
+
+
+                except sr.UnknownValueError:
+                    print(resposta_erro_aleatoria)
+                    sai_som(resposta_erro_aleatoria)
+            break
+
+    print("="* len(apresentacao))
+    print("Ouvindo.....")
+    while True:
+        resposta_erro_aleatoria = choice(lista_erros)
+        rec = sr.Recognizer()
+        with sr.Microphone() as s:
+            rec.adjust_for_ambient_noise(s)
+
+            while True:
+                try:
+                    audio = rec.listen(s)
+                    entrada = rec.recognize_google(audio, language="pt-br")
+                    print("{}: {}".format(user_name, entrada))
+
+                    #operações matemáticas
+                    if "Quanto é" in entrada or "quanto é" in entrada:
+                        entrada = entrada.replace("Quanto é", "")
+                        resposta = calcula(entrada)
+
+                    # Pede Tempo
+                    elif "temperatura" in entrada:
+
+                        lista_tempo = temperatura()
+                        temp = lista_tempo[0]
+                        temp_max = lista_tempo[1]
+                        temp_min = lista_tempo[2]
+
+                        resposta = "A temperatura de hoje é {:.2f}°. Temos a máxima de {:.2f}° e uma minima de {:.2f}°".format(temp,  temp_max, temp_min)
+                    
+                    # Informações da cidade
+                    elif "informações" in entrada and "cidade" in entrada:
+
+                        resposta = "Mostrando informações da cidade"
+                    
+                    else:
+                        resposta = conversas[entrada]
+
+                    if resposta == "Mostrando informações da cidade":
+                        #mostra informações da cidade
+
+                        lista_infos = clima_tempo()
+                        longitude = lista_infos[0]
+                        latitude = lista_infos[1]
+                        temp = lista_infos[2]
+                        pressao_atm = lista_infos[3]
+                        humidade = lista_infos[4]
+                        temp_max = lista_infos[5]
+                        temp_min = lista_infos[6]
+                        v_speed = lista_infos[7]
+                        v_direc = lista_infos[8]
+                        nebulosidade = lista_infos[9]
+                        id_da_cidade = lista_infos[10]
+
+                        print("Assistente:")
+                        print("Mostrando informações de {}\n\n".format(cidade))
+                        sai_som("Mostrando informações de {}".format(cidade))
+                        print("Longitude: {}, Latitude: {}\nId: {}\n".format(longitude, latitude, id_da_cidade))
+                        print("Temperatura : {:.2f}°".format(temp))
+                        print("Temperatura Máxima : {:.2f}°".format(temp_max))
+                        print("Temperatura Mínima : {:.2f}°".format(temp_min))
+                        print("Humidade: {}".format(humidade))
+                        print("Nebulosidade: {}".format(nebulosidade))
+                        print("Velocidade do vento: {}m/s\nDireção do vento: {}".format(v_speed,v_direc))
+
+                    else:
+                        print('Assistente: {}'.format(resposta))
+                        sai_som('{}'.format(resposta))
+
+                except sr.UnknownValueError:
+                    print(resposta_erro_aleatoria)
+                    sai_som(resposta_erro_aleatoria)
+
+if __name__ == '__main__':
+    intro()
+    sai_som("iniciando")
+    assistente()
+
+
+
+
